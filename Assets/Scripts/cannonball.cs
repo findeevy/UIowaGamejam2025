ee12ccb18261866101461f734f9e68442fb61c0d
Aaron Grace <aarongraceart@gmail.com>
2025-04-12 17:58:19 -0500

    init

--- Assets/Scripts/cannonball.cs
+++ Assets/Scripts/cannonball.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class cannonball : MonoBehaviour
@@ -6,17 +7,51 @@ public class cannonball : MonoBehaviour
     private Rigidbody rb;
     public GameObject gameObject;
 
+    private Transform ball;
+
     void Start(){
+
         //Launch the ball at a given velocity.
         rb = GetComponent<Rigidbody>();
         Vector3 launchDirection = transform.forward;
 
         rb.velocity = launchDirection * launchSpeed * PlayerPrefs.GetInt("speed") * 0.5f;
+
+        // gameObject.GetComponent<Collider>().enabled = false;
+        ball = transform.GetChild(0);
+        if (ball != null)
+        {
+            ball.gameObject.GetComponent<Collider>().enabled = false;
+            Invoke(nameof(ReenableCollision), 0.5f); // Re-enable the collider after 0.5 seconds
+        }
+
+
         //Clean up stray balls.
         Destroy(gameObject, 5f);
+
+    }
+    void ReenableCollision(){
+        //Re-enable the collider after a delay.
+        if (ball != null)
+        {
+            ball.gameObject.GetComponent<Collider>().enabled = true;
+        }
     }
 
     void OnCollisionEnter(Collision collision){
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy")){
+            //If the ball hits an enemy, apply damage.
+            EnemyBehavior enemy = collision.gameObject.GetComponent<EnemyBehavior>();
+            if (enemy != null)
+            {
+                Vector3 direction = (collision.transform.position - transform.position).normalized;
+                enemy.TakeDamage(20, direction);
+            }
+
+        }
+
+
         //Destroy the cannonball on hit.
          foreach (Transform child in transform)
          {
